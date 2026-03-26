4d773e4425a7d85c72e12cb7bad9903911b8e47b
JJamC <jaycarroll97@live.co.uk>
2026-03-26 07:04:12 +0000

    completed contact component

--- src/pages/components/Contact.tsx
+++ src/pages/components/Contact.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactForm() {
-
     const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
   if (state.succeeded) {
     return <p>Message Sent!</p>;
@@ -27,7 +26,7 @@ export default function ContactForm() {
                 id="first-name"
                 type="text"
                 name="first-name"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-[#304620] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               />
             </div>
           </div>
@@ -38,7 +37,7 @@ export default function ContactForm() {
                 id="last-name"
                 type="text"
                 name="last-name"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-[#304620] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               />
             </div>
           </div>
@@ -49,7 +48,7 @@ export default function ContactForm() {
                 id="email"
                 type="email"
                 name="email"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-[#304620] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               />
             </div>
           </div>
@@ -59,7 +58,7 @@ export default function ContactForm() {
               <textarea
                 id="message"
                 name="message"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-[#304620] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
               ></textarea>
             </div>
           </div>
@@ -76,58 +75,3 @@ export default function ContactForm() {
     </div>
   );
 }
-
-
-// export default function ContactForm() {
-//   const [state, handleSubmit] = useForm("mojpqwqv");
-//   if (state.succeeded) {
-//     return <p>Message Sent!</p>;
-//   }
-//   return (
-//     <form
-//       action="https://formspree.io/f/{FORM_ID}"
-//       method="POST"
-//       className="grid place-items-center gap-4"
-//     >
-//       <div className="flex flex-col gap-2">
-//         <label className="text-sm">Your Name</label>
-//         <input
-//           id="name"
-//           name="name"
-//           required
-//           className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-//         />
-//       </div>
-
-//       <div className="flex flex-col gap-2">
-//         <label className="text-sm">Email</label>
-//         <input
-//           id="email"
-//           name="email"
-//           required
-//           className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-//         />
-//       </div>
-//           <div className="sm:col-span-2">
-//         <label className="block text-sm/6 font-semibold">Message</label>
-//         <div className="mt-2.5">
-//         <textarea
-//           id="message"
-//           name="message"
-//           required
-//           className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-//         ></textarea>
-//         </div>
-//         </div>
-
-//       <div className="flex justify-end">
-//         <button
-//           type="submit"
-//           className="rounded-full bg-[#44622D] px-5 text-lg font-semibold transition-colors duration-200 hover:bg-[#304620] focus:ring-4 focus:ring-[#fdcf4c] focus:outline-none"
-//         >
-//           Submit
-//         </button>
-//       </div>
-//     </form>
-//   );
-// }
