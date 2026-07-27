ecc4d9ac766b61201baca5960309cac5304f35f6
JJamC <jaycarroll97@live.co.uk>
2026-07-27 11:12:18 +0100

    added bobby to tracks

--- src/pages/components/Projects.tsx
+++ src/pages/components/Projects.tsx
@@ -1,3 +1,36 @@
 export default function Production() {
-    return <p> Coming soon...! :)</p>
+    return (
+      <div>
+        <div className="mb-5 flex flex-col justify-center md:flex-row">
+          <iframe
+            className="aspect-[16/7] md:w-250"
+            src={
+              "https://www.youtube.com/embed/XYyqHmqNsCo?si=FTBWtOwsBG0olfeh"
+            }
+            title="YouTube video"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
+        <p className="p-5 md:mx-15">
+          <b>Bobby |</b> The fantastical adventure of a curious runaway dog
+          
+          <br></br>
+          Bobby is a personal nostalgia project with clear influences from
+          electronic dance and video game music.
+          The visuals are a nod to the pixel art popular in the video games of
+          the 1990s that I grew up on. <br></br> <br></br>
+          Music produced in Ableton and visuals animated in Pixelorama. <br></br>
+          <br></br>
+          <a
+            href="https://soundcloud.com/kyarvo/sets/bobby"
+            target="_blank"
+            rel="noopener noreferrer"
+                >
+                    <b>Click here to listen in SoundCloud</b>
+          </a>
+        
+        </p>
+      </div>
+    );
 }
